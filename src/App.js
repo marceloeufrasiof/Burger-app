274fbfd717669e374c91c3ae5d38ae04054db876
marceloeufrasiof <marceloeufrasiof@gmail.com>
2023-05-17 22:14:39 -0300

    atualização na estilização

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
 import React from "react";
-import { Container, Image, H1, ContainerItens, InputLabel, Input, Button, Body } from "./styles";
-import Image1 from "./Assets/burger1.png"
+import {
+  Container,
+  Image,
+  H1,
+  ContainerItens,
+  InputLabel,
+  Input,
+  Button,
+  Request,
+} from "./styles";
+import Image1 from "./Assets/burger1.png";
+import Arrow from "./Assets/arrow.svg";
+import Trash from "./Assets/trash.svg";
 
 function App() {
+  const requests = [
+    { id: Math.random(), order: "X-tudo", name: "Marcelo" },
+    { id: Math.random(), order: "Eggs X-salada", name: "Gislayne" },
+  ];
+
   return (
-    
     <Container>
-      <Image src={Image1} alt="Burger"/>
+      <Image src={Image1} alt="Burger" />
 
       <H1> Faça seu pedido! </H1>
 
       <ContainerItens>
+        <InputLabel> Pedido </InputLabel>
 
-      <InputLabel> Pedido </InputLabel>
+        <Input placeholder="1 Coca-Cola, 1-X Salada "></Input>
 
-      <Input placeholder="1 Coca-Cola, 1-X Salada "></Input>
+        <InputLabel> Nome do cliente </InputLabel>
 
-      <InputLabel> Nome do cliente </InputLabel>
+        <Input placeholder="Steve Jobs"></Input>
 
-      <Input placeholder="Steve Jobs"></Input>
+        <Button>
+          Novo Pedido <img src={Arrow} alt="arrow" />
+        </Button>
+
+        <ul>
+          {requests.map((request) => (
+            <Request key={request.id}>
+              <p>{request.order}</p> <p>{request.name}</p>
+              <button>
+                <img src={Trash} alt="trash" />
+              </button>
+            </Request>
+          ))}
+        </ul>
       </ContainerItens>
-
-      <Button> Novo Pedido </Button>
-
     </Container>
   );
 }
--- src/styles.js
+++ src/styles.js
@@ -5,6 +5,7 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 25px;
+  height: 100vh;
 `;
 
 export const Image = styled.img`
@@ -14,6 +15,8 @@ export const Image = styled.img`
 export const H1 = styled.h1`
   font-size: 28px;
   color: #ffffff;
+  font-style: normal;
+  font-weight: 700;
 `;
 
 export const ContainerItens = styled.div`
@@ -21,11 +24,15 @@ export const ContainerItens = styled.div`
   color: white;
   display: flex;
   flex-direction: column;
+  padding: 50px 36px;
 `;
 
 export const InputLabel = styled.p`
   font-size: 18px;
   color: #eeeeee;
+  padding-left: 15px;
+  font-style: normal;
+  font-weight: 700;
 `;
 
 export const Input = styled.input`
@@ -34,16 +41,99 @@ export const Input = styled.input`
   background: rgba(255, 255, 255, 0.25);
   border-radius: 14px;
   border: none;
+  outline: none;
 
   font-size: 18px;
   color: #ffffff;
+  padding-left: 15px;
+  margin-bottom: 42px;
+  font-style: normal;
+  font-weight: 300;
+
+  &::placeholder {
+    color: white;
+  }
 `;
 
 export const Button = styled.button`
   width: 342px;
   height: 68px;
-  background: #d93856;
+  background: linear-gradient(to right, #d93856, rgb(74, 39, 110));
 
   font-size: 17px;
   color: #ffffff;
+  border: none;
+  outline: none;
+
+  font-style: normal;
+  font-weight: 900;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  cursor: pointer;
+  border-radius: 14px;
+
+  &:hover {
+    animation-name: buttonAnimation;
+    animation-duration: 0.5s;
+    animation-iteration-count: infinite;
+    animation-direction: alternate;
+    animation-timing-function: ease-in-out;
+    box-shadow: 0 0 50px 20px rgba(74, 82, 110, 0.5);
+  }
+
+  &:active {
+    opacity: 0.6;
+  }
+
+  @keyframes buttonAnimation {
+    0% {
+      transform: scale(1);
+    }
+
+    100% {
+      transform: scale(1.05);
+    }
+  }
+`;
+
+export const Request = styled.li`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  width: 342px;
+  height: 58px;
+  background: rgba(255, 255, 255, 0.25);
+  border-radius: 14px;
+  border: none;
+  outline: none;
+  font-size: 18px;
+  color: #ffffff;
+  font-style: normal;
+  font-weight: 300;
+  margin-top: 15px;
+
+  p {
+    font-style: normal;
+    font-weight: 300;
+    font-size: 18px;
+    line-height: 21px;
+    color: #ffffff;
+  }
+
+  button{
+    background: none;
+    border: none;
+    cursor: pointer;
+  }
+
+  button:hover{
+    opacity: 0.6;
+  }
+
+  button:active{
+    opacity: 0.4;
+  }
 `;
